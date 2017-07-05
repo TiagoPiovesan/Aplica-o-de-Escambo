1e101abe2bc666eb89ab84e2b9be0ae856f3efcb
TiagoPiovesan <tiago.piovesan.tp@gmail.com>
2017-07-05 22:57:19 +0000

    Ambiente configurado / logins com devise / seeds de categorias / modelos criados: admin - category - member

---
+++ db/seeds.rb
@@ -0,0 +1,26 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+puts "Cadastrando as CATEGORIAS..."
+
+categories = [ "Animais e Acessórios",
+              "Esportes",
+              "Para sua casa",
+              "Eletrônicos e Celulares",
+              "Música e hobbies",
+              "Bebês e crianças",
+              "Moda e Beleza",
+              "Veículos e barcos",
+              "Imóveis",
+              "Empregos e negócios" ]
+
+categories.each do |category|
+  Category.find_or_create_by(description:category)
+end
+
+puts "Categorias cadastradas com sucesso!"
