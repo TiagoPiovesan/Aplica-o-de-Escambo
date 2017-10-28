0f85ddf735591034d84d75158fc4fda18c84a138
TiagoPiovesan <tiago.piovesan.tp@gmail.com>
2017-10-28 01:29:21 +0000

    conf Capstrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,8 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.10.0"
 
+require "bundler/capistrano"
+
 set :application, "EscamboApp"
 set :repo_url, "https://github.com/TiagoPiovesan/Aplicao-de-Escambo_Rails.git"
 
